fdfd5757e2ebcc0e56079a97e0c15c66503801f8
pingxingkongjian <153248967@qq.com>
2017-04-04 08:19:59 +0800

    Update

--- src/main/java/java1702/javase/io/InputStreamTest.java
+++ src/main/java/java1702/javase/io/InputStreamTest.java
@@ -30,3 +30,4 @@ public class InputStreamTest {
         }
     }
 }
+
--- src/main/java/java1702/javase/practice/E1.java
+++ src/main/java/java1702/javase/practice/E1.java
@@ -7,5 +7,14 @@ package java1702.javase.practice;
 public class E1 {
     public static void main(String[] args) {
 
+        for (int j = 1; j < 10; j++) {
+            for (int k = 1; k <10; k++) {
+                if (j >= k) {
+
+                System.out.print(j + "*" + k + "=" + j*k +"\t");
+                }
+            }
+        System.out.println("---");
+        }
     }
 }
