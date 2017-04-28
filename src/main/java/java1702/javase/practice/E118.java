5c4ba9812095486308ac3d292a7a81eb7e1275aa
pingxingkongjian <153248967@qq.com>
2017-04-28 10:33:57 +0800

    Update

--- src/main/java/java1702/javase/practice/E118.java
+++ src/main/java/java1702/javase/practice/E118.java
@@ -6,22 +6,22 @@ import java.util.Arrays;
  * Created by 刘晋勇 on.
  * 2017/4/15  9:24.
  */
+// 拆分字符串，字符串中含有逗号和数字，对数字排序并输出
+//        例如：String s0=“123,45,25,85”;
+//        输出：25 45 85 123
+//        sort
 public class E118 {
     public static void main(String[] args) {
         String s = "123,45,25,85";
 
-        String[] s1 = s.split(",");
-
-        int[] a = new int[s1.length];
-
-        for (int i = 0; i < a.length; i++) {
-            a[i] = Integer.parseInt(s1[i]);
-
+        String[] strings = s.split(",");
+        int[] ints = new int[strings.length];
+        for (int i = 0; i < strings.length; i++) {
+            ints[i] = Integer.parseInt(strings[i]);
         }
-
-        Arrays.sort(a);
-        for (int i1 : a) {
-            System.out.println(i1 + "");
+        Arrays.sort(ints);
+        for (int anInt : ints) {
+            System.out.println(anInt + "");
         }
     }
 }
