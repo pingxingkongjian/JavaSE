8ff0581187daaf8e3159e9f0ab6025e51da0c98d
pingxingkongjian <153248967@qq.com>
2017-05-10 15:08:54 +0800

    Update

--- src/main/java/java1702/javase/multithreading/Account.java
+++ src/main/java/java1702/javase/multithreading/Account.java
@@ -7,24 +7,32 @@ package java1702.javase.multithreading;
 public class Account {
     private int money;
 
-    public void withdraw() {
+    public Account(int money) {
+        this.money = money;
+    }
 
+    public void withdraw(int money) {
+        if (this.money - money < 0) {
+            System.out.println("Not enough...");
+            return;
+        }
+        this.money -= money;
     }
 
     public int getmoney() {
         return money;
     }
-
-    public static void main(String[] args) {
-        Thread thread = new Thread(new AccountTest());
-
-
-    }
 }
 
 class AccountTest implements Runnable {
+    private Account account;
+
     @Override
     public void run() {
+        account.withdraw(1000);
+    }
+
+    public static void main(String[] args) {
 
     }
 }
